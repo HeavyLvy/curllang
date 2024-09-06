20eba8572fa8dd1e2b5a9a4a464807ed9a9598a9
HeavyLvy <therealheavylvy@gmail.com>
2024-09-06 00:26:02 -0600

    code  formating.  Added .ruff_cache to .gitignore

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def parse_line(line: str):
     def raise_parse_error(message: str, token_index, error_code=-1):
         nonlocal error_found
         error_found = True
-        result['error'] = {
-            'message': message,
-            'code': error_code,
-            'token_index': token_index - 1
+        result["error"] = {
+            "message": message,
+            "code": error_code,
+            "token_index": token_index - 1,
             # We subtract one because the char still gets proccsed after the error is called, incrementing the index by one. (which would be the next index, that we dont want).
         }
         error_code = error_code
@@ -31,66 +31,68 @@ def parse_line(line: str):
         def append_token():
             nonlocal token_sequence, flag
 
-            parsed_tokens.append({'flag': flag, 'value': ''.join(token_sequence)})
+            parsed_tokens.append({"flag": flag, "value": "".join(token_sequence)})
             flag = None
             token_sequence = []
 
         # Set Flags
         if not flag:
             if char in string.ascii_letters:
-                flag = 'identifier'
+                flag = "identifier"
             if char in string.digits:
-                flag = 'integer'
-            if char == '+':
-                flag = 'addition'
-            if char == '-':
-                flag = 'subtraction'
-            if char == '*':
-                flag = 'multiplication'
-            if char == '/':
-                flag = 'division'
-            if char == '=':
-                flag = 'assignment'
+                flag = "integer"
+            if char == "+":
+                flag = "addition"
+            if char == "-":
+                flag = "subtraction"
+            if char == "*":
+                flag = "multiplication"
+            if char == "/":
+                flag = "division"
+            if char == "=":
+                flag = "assignment"
 
         # Handle Flags
-        if flag == 'identifier':
-            if char not in string.ascii_letters + '_' and char not in string.digits:
+        if flag == "identifier":
+            if char not in string.ascii_letters + "_" and char not in string.digits:
                 append_token()
             else:
                 token_sequence.append(char)
-        if flag == 'integer':
-            if char not in string.digits or char == '.':
-                if char == '.':
-                    flag = 'float'
+        if flag == "integer":
+            if char not in string.digits or char == ".":
+                if char == ".":
+                    flag = "float"
                 else:
                     append_token()
             else:
                 token_sequence.append(char)
-        if flag == 'float':
-            if token_sequence.count('.') > 1:
-                raise_parse_error('No such data type with more than 1 dot.', i, 3)
-            if char not in string.digits and char != '.':
+        if flag == "float":
+            if token_sequence.count(".") > 1:
+                raise_parse_error("No such data type with more than 1 dot.", i, 3)
+            if char not in string.digits and char != ".":
                 append_token()
             else:
                 token_sequence.append(char)
-        if flag == 'addition':
+        if flag == "addition":
             token_sequence.append(char)
             append_token()
-        if flag == 'subtraction':
+        if flag == "subtraction":
             token_sequence.append(char)
             append_token()
-        if flag == 'multiplication':
+        if flag == "multiplication":
             token_sequence.append(char)
             append_token()
-        if flag == 'division':
+        if flag == "division":
             token_sequence.append(char)
             append_token()
-        if flag == 'assignment':  # Handles both assignment and comparison
-            if char != '=':
+        if flag == "assignment":  # Handles both assignment and comparison
+            if char != "=":
                 if len(token_sequence) == 2:
-                    flag = 'comparison'
+                    flag = "comparison"
                 elif len(token_sequence) > 2:
-                    raise_parse_error('No such operation for more than 2 equal signs', i, 2)
+                    raise_parse_error(
+                        "No such operation for more than 2 equal signs", i, 2
+                    )
 
                 append_token()
             else:
@@ -100,7 +102,7 @@ def parse_line(line: str):
         if flag and i == len(line) - 1:
             append_token()
 
-    result['tokens'] = parsed_tokens
+    result["tokens"] = parsed_tokens
     return result
 
 
@@ -113,69 +115,84 @@ def get_last_five_lines(text):
 
 
 def parse_code(input_code: str, verbose: int = 0):
-    processed_code = ''
+    processed_code = ""
     found_errors = []
 
     # Go through each line of the code and parse and put any errors into the found errors list
     for line_number, line_content in enumerate(input_code.splitlines()):
-        processed_code += line_content + '\n'
+        processed_code += line_content + "\n"
         parsed_line = parse_line(line_content)
 
-        if parsed_line.get('error'):
+        if parsed_line.get("error"):
             syntax = Syntax(
-                get_last_five_lines(processed_code[1: -1]),
-                "lua", theme="monokai",
+                get_last_five_lines(processed_code[1:-1]),
+                "lua",
+                theme="monokai",
                 line_numbers=True,
                 highlight_lines=[line_number],  # NOQA
-                start_line=len(processed_code.splitlines()) - 5 if len(processed_code.splitlines()) > 5 else 1,
+                start_line=len(processed_code.splitlines()) - 5
+                if len(processed_code.splitlines()) > 5
+                else 1,
             )
-            found_errors.append({'error': parsed_line['error'], 'code': syntax, 'line': line_number})
-        elif parsed_line['tokens']:
-            console.print(parsed_line['tokens'])
+            found_errors.append(
+                {"error": parsed_line["error"], "code": syntax, "line": line_number}
+            )
+        elif parsed_line["tokens"]:
+            console.print(parsed_line["tokens"])
 
     # Output the errors
     if found_errors:
         console.print(
             f"[white on red bold]{len(found_errors)} parsing error{'s' if len(found_errors) > 1 else ''} raised.",
-            highlight=False)
+            highlight=False,
+        )
         for error_index, error in enumerate(found_errors, 1):
-            token_index = error['error']['token_index']
+            token_index = error["error"]["token_index"]
 
             console.print(
                 f"[[white]{error_index}[red]] {ERROR_CODES[error['error']['code']]['message']} Error Code: {error['error']['code']}",
-                style="bold red", highlight=False)
+                style="bold red",
+                highlight=False,
+            )
             if verbose > 0:
-                console.print(f'    {error['error']['message']}', style="bold red", highlight=False)
+                console.print(
+                    f'    {error['error']['message']}',
+                    style="bold red",
+                    highlight=False,
+                )
             if verbose > 1:
-                console.print(f"    {ERROR_CODES[error['error']['code']]['description']}", style="bold red",
-                              highlight=False)
-            console.print(error['code'])
-            console.print(' ' * (token_index + 3 + len(str(error['line']))) + '^', style='bold yellow')
+                console.print(
+                    f"    {ERROR_CODES[error['error']['code']]['description']}",
+                    style="bold red",
+                    highlight=False,
+                )
+            console.print(error["code"])
+            console.print(
+                " " * (token_index + 3 + len(str(error["line"]))) + "^",
+                style="bold yellow",
+            )
 
 
 ERROR_CODES = {
     -1: {
-        'message': 'Exception Occured.',
-        'description': 'An exception, can be casued by different reasons.'
+        "message": "Exception Occured.",
+        "description": "An exception, can be casued by different reasons.",
     },
-    1: {
-        'message': 'Invalid Syntax.',
-        'description': 'Caused by inproper syntax.'
-    },
+    1: {"message": "Invalid Syntax.", "description": "Caused by inproper syntax."},
     2: {
-        'message': 'Invalid Syntax.',
-        'description': 'Caused if there is more than 2 equal signs in the expression eg "x === y", "1 ======== 53".'
+        "message": "Invalid Syntax.",
+        "description": 'Caused if there is more than 2 equal signs in the expression eg "x === y", "1 ======== 53".',
     },
     3: {
-        'message': 'Invalid Syntax.',
-        'description': 'Caused if there is more than 1 dot found in a floating number.'
-    }
+        "message": "Invalid Syntax.",
+        "description": "Caused if there is more than 1 dot found in a floating number.",
+    },
 }
 
-example_code = '''
+example_code = """
 1 === 2
 print("hello")
 print(1.03435.123214)
-'''
+"""
 
 parse_code(example_code)
