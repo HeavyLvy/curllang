37d11893e964d54c80f9f58d577d189683125a79
HeavyLvy <therealheavylvy@gmail.com>
2024-09-07 17:43:31 -0600

    Added doc striing and renamed arg text to string

--- jura/common.py
+++ jura/common.py
@@ -1,6 +1,13 @@
-def get_last_five_lines(text: str):
-    lines = text.splitlines()
+def get_last_five_lines(string: str) -> str:
+    """
+    :param string: The string to get the last five-lines.
+    :type string: String
+
+    :return: A string containing the last five lines of the string argument.
+    :rtype: String
+    """
+    lines = string.splitlines()
     if len(lines) > 5:
         return '\n'.join(lines[-5:])
     else:
-        return text
+        return string
